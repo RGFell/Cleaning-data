e29567d7dc9471c3b8b5f2432afef2e66f6f7cf0
dave <dunnedave+coursera@gmail.com>
2015-03-07 08:17:19 -0800

    Refactored Step 3 in run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,20 +1,14 @@
 # Load libraries
-library("data.table")
-library("reshape2")
+library('data.table')
+library('reshape2')
 
 # Read in training and test sets.
-X.train.set <- read.table("UCI_HAR_Dataset//train//X_train.txt")
-Y.train.set <- read.table("UCI_HAR_Dataset//train//y_train.txt")
-subject.train.set <- read.table("UCI_HAR_Dataset//train//subject_train.txt")
-X.test.set <- read.table("UCI_HAR_Dataset//test//X_test.txt")
-Y.test.set <- read.table("UCI_HAR_Dataset//test//y_test.txt")
-subject.test.set <- read.table("UCI_HAR_Dataset//test//subject_test.txt")
-
-# [Assignment step 3] 
-# Use descriptive activity names to name the activities in the data set 
-activity.labels <- read.table("UCI_HAR_Dataset//activity_labels.txt")[, 2] # activity data is in second column
-Y.train.set[, 2] = activity.labels[Y.train.set[, 1]]
-Y.test.set[, 2] = activity.labels[Y.test.set[, 1]]
+X.train.set <- read.table('UCI_HAR_Dataset//train//X_train.txt')
+Y.train.set <- read.table('UCI_HAR_Dataset//train//y_train.txt')
+subject.train.set <- read.table('UCI_HAR_Dataset//train//subject_train.txt')
+X.test.set <- read.table('UCI_HAR_Dataset//test//X_test.txt')
+Y.test.set <- read.table('UCI_HAR_Dataset//test//y_test.txt')
+subject.test.set <- read.table('UCI_HAR_Dataset//test//subject_test.txt')
 
 # [Assignment step 1] 
 # Combine training and test sets into one data set using cbind and rbind. 
@@ -25,15 +19,22 @@ all.data <- rbind(all.train.data, all.test.data)
 # [Assignment step 2] 
 # Extract only the measurements on the mean and standard deviation for each measurement.
 
-# First, update column names
-feature.names <- read.table("UCI_HAR_Dataset//features.txt", stringsAsFactors = FALSE ) 
-column.names <- c("subject.id", "activity", "activity.labels", feature.names[, 2]) # feature names data is in second column
-names(all.data) <- column.names
+# Step 2.1 First, update column names
+feature.names <- read.table('UCI_HAR_Dataset//features.txt', stringsAsFactors = FALSE ) 
+column.names <- c('subject.id', 'activity', feature.names[, 2]) # feature names data is in second column
 
-# Then create a dataset called all.mean.std.data which contains only mean and standard deviation columns. 
+names(all.data) <- column.names # Update the column names
+
+# Step 2.2 Then create a dataset called all.mean.std.data which contains only mean and standard deviation columns. 
 keep.columns <- grepl('mean|std', colnames(all.data))  # Columns (Mean or Standard Deviation will be kept)
-keep.columns[1:3] <- TRUE # Also ensure we keep subject.id, acitity and activity.label
+keep.columns[1:2] <- TRUE # Also ensure we keep subject.id, acitity
 all.mean.std.data <- all.data[, keep.columns]
+
+# [Assignment step 3] 
+# Use descriptive activity names to name the activities in the data set 
+activity.labels <- read.table('UCI_HAR_Dataset//activity_labels.txt')[, 2] # activity data is in second column
+all.mean.std.data[, 2] = activity.labels[all.mean.std.data[, 2]]  # Replace column 2 (activity) based on the activity label values
+
 
 # [Assignment step 4] 
 # Appropriately labels the data set with descriptive variable names  
@@ -55,10 +56,10 @@ names(all.mean.std.data) <- column.names  # write back the new columns name to a
 # [Assignment step 5]
 # From the data set in step 4, create a second, independent tidy data set with the 
 # average of each variable for each activity and each subject.
-melted.data <- melt(all.mean.std.data, id=c("subject.id","activity.labels"))
-tidy.data <- dcast(melted.data, subject.id + activity.labels ~ variable, mean)  # recast the melted data into a mean of each vairable.
+melted.data <- melt(all.mean.std.data, id=c('subject.id','activity')) #Melt data into DF of activity and sbject.
+tidy.data <- dcast(melted.data, subject.id + activity ~ variable, mean)  # recast the melted data into a mean of each vairable.
 
 # Write the new tiday data to a file.
-write.table(tidy.data, "tidy.data.txt", sep=",", row.names = FALSE)  # Write the tidy data to a txt file.
+write.table(tidy.data, 'tidy.data.txt', sep=',', row.names = FALSE)  # Write the tidy data to a txt file.
 
 
